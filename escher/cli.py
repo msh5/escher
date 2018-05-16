49874dcd6102b30a7b4b4c66cd92d2078d392117
Sho Minagawa <msh5.global@gmail.com>
2018-05-16 23:20:15 +0900

    Use CLI framework 'Click'

---
+++ escher/cli.py
@@ -0,0 +1,24 @@
+import click
+import elasticsearch
+
+from escher import __version__
+
+
+@click.group()
+@click.version_option(version=__version__, message='escher %(version)s')
+def cli():
+    pass
+
+
+@click.command()
+@click.option('-v', '--verbose', is_flag=True)
+def cat(verbose):
+    'Call "cat" APIs.'
+    click.echo('foo')
+
+
+cli.add_command(cat)
+
+
+def main():
+    cli()
