bc23a7af35a68290a9f41d50db377cce9550c4e1
Sho Minagawa <msh5.global@gmail.com>
2018-05-19 14:47:58 +0900

    Try to support 'health' subcommand

--- escher/cmd/escher.py
+++ escher/cmd/escher.py
@@ -1,23 +1,32 @@
 import click
 
 from escher import __version__
+from elasticsearch import Elasticsearch
+from tabulate import tabulate
 
 
 @click.group()
+@click.option('--host', '-h', default='localhost')
+@click.option('--port', '-p', type=int, default='80')
+@click.option('--ssl/--no-ssl', default=False)
 @click.version_option(version=__version__, message='escher %(version)s')
-def cli():
-    pass
+@click.pass_context
+def cli(ctx, host, port, ssl):
+    ctx.obj['host_spec'] = {'host': host, 'port': port, 'use_ssl': ssl}
 
 
 @click.command()
-@click.option('-v', '--verbose', is_flag=True)
-def cat(verbose):
-    'Call "cat" APIs.'
-    click.echo('foo')
+@click.pass_context
+def health(ctx):
+    '''Check cluster health'''
+    host = ctx.obj['host_spec']
+    client = Elasticsearch(hosts=[host])
+    resp = client.cluster.health()
+    click.echo(tabulate(resp.items()))
 
 
-cli.add_command(cat)
+cli.add_command(health)
 
 
 def main():
-    cli()
+    cli(obj={})
